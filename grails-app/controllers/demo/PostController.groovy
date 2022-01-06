ff5fe873f04e5d1d6fa9aee0d21e671dc37431f8
Michael Yan <rain@rainboyan.com>
2022-01-06 18:06:08 +0800

    Grails scaffolding and fields demo

---
+++ grails-app/controllers/demo/PostController.groovy
@@ -0,0 +1,7 @@
+package demo
+
+class PostController {
+
+    static scaffold = Post
+
+}
---
+++ grails-app/domain/demo/Post.groovy
@@ -0,0 +1,28 @@
+package demo
+
+class Post {
+    String title
+    String body
+    Status status
+    String press
+    Integer num
+    Date publishedDate
+    Boolean isPrivate = true
+
+    static constraints = {
+        title(blank: false)
+        body(blank: false, widget: 'textarea')
+        num(nullable: true, range: 1..10)
+        status(nullable: false)
+        press inList: ['MIT', 'Amazon']
+        publishedDate(nullable: false)
+        isPrivate(nullable: false)
+    }
+}
+
+enum Status { 
+    DRAFT(1), PRIVATE(2), PENDING(3), PUBLISH(4)
+    Status(int value) { this.value = value }
+    private final int value
+    public int value() { return value }
+}
